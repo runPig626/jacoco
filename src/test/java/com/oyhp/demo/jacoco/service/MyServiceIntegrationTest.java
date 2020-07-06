aab4df4723fe406da5668c124af37145281ac785
欧阳海平 <13539111482@163.com>
2020-07-06 16:36:04 +0800

    feat: integration test, mock docker

---
+++ src/test/java/com/oyhp/demo/jacoco/service/MyServiceIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.oyhp.demo.jacoco.service;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * XXXX
+ *
+ * @author OuYangHaiPing<OuYanghp @ pvc123.com>
+ * @date 2020-07-06
+ */
+class MyServiceIntegrationTest {
+    @Test
+    void test(){
+        System.out.println("I am integrationTest");
+    }
+}
