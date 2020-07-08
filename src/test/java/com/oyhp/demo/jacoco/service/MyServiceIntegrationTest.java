219ff7e64e7afb97a4b6d20672771d1bc373164c
欧阳海平 <13539111482@163.com>
2020-07-08 17:22:40 +0800

    feat(test_controller): integrate test controller

--- src/test/java/com/oyhp/demo/jacoco/service/MyServiceIntegrationTest.java
+++ src/test/java/com/oyhp/demo/jacoco/service/MyServiceIntegrationTest.java
@@ -1,9 +1,14 @@
 package com.oyhp.demo.jacoco.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.oyhp.demo.jacoco.JacocoApplication;
 import com.oyhp.demo.jacoco.model.Category;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.configuration.ClassicConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +16,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 /**
@@ -24,8 +35,11 @@ import java.util.List;
 @SpringBootTest(classes = JacocoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:test.properties")
 class MyServiceIntegrationTest {
+    private MockMvc mockMvc;
     @Autowired
     CategoryService categoryService;
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
     @BeforeEach
     void setUp(){
@@ -40,11 +54,32 @@ class MyServiceIntegrationTest {
 //        Flyway flyway = new Flyway(conf);
 //        flyway.clean();
 //        flyway.migrate();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
-    void test(){
+    void fillAllTest(){
         List<Category> categoryList = categoryService.fillAll();
         System.out.println("categoryList = " + categoryList);
     }
+
+    @Test
+    void do_get_list_test(){
+        try {
+            MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/category/list"))
+                                .andExpect(MockMvcResultMatchers.status().is(200))
+                                .andDo(MockMvcResultHandlers.print())
+                                .andReturn();
+            int status = mvcResult.getResponse().getStatus();
+            Assertions.assertEquals(200, status);
+            String result = mvcResult.getResponse().getContentAsString();
+            JSONArray resultArray = JSON.parseArray(result);
+            // 判断接口返回json中success字段是否为true
+            Assertions.assertEquals(4, resultArray.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
